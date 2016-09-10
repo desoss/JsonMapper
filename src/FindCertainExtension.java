d94cdd51573b3a006302be61509399e00ef4a2a0
jacoporigoli <jackeape@gmail.com>
2016-09-10 15:32:36 +0200

    Very Quick AndVery Dirty version

---
+++ src/FindCertainExtension.java
@@ -0,0 +1,150 @@
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+
+import it.polimi.diceH2020.SPACE4Cloud.shared.inputData.InstanceData;
+import it.polimi.diceH2020.SPACE4Cloud.shared.inputDataMultiProvider.InstanceDataMultiProvider;
+import it.polimi.diceH2020.SPACE4Cloud.shared.inputDataMultiProvider.JobMLProfilesMap;
+
+public class FindCertainExtension {
+
+	/* EVERY INPUT JSON FILE MUST HAVE THE EXTENSION .json IN ITS NAME*/
+	private static final String FILE_INPUT_DIR = "#@INPUTFOLDERPATH@#";
+	private static final String FILE_TEXT_EXT = ".json";
+	private static final String FILE_OUTPUT_DIR = "#@OUTPUTFOLDERPATH@#";
+		
+	/*  If ADD_ML_FEATURES = true, MLProfile are added from .json files contained in FILE_ML_DIR.
+		Every external .json file (containing the MLFeatures) should named with ApplicationID_.
+		For example if Application id is ID123, so the file should be named ID123_.json  */
+	private static final boolean ADD_ML_FEATURES = true; 
+	private static final String FILE_ML_DIR = "#@MLFOLDERPATH@#";
+	
+	ObjectMapper mapper = new ObjectMapper().registerModule(new Jdk8Module());
+	private String[] list;
+	
+	public static void main(String[] args) {
+		FindCertainExtension findExt = new FindCertainExtension();
+		checkInputs();
+		findExt.listFile(FILE_INPUT_DIR, FILE_TEXT_EXT);
+		try {
+			findExt.convertJSONs(ADD_ML_FEATURES,FILE_ML_DIR);
+		} catch (IOException e) {
+			System.out.println("Error with JSONs serialization!");
+		}
+	}
+	
+	private static void checkInputs(){
+		if(new File(FILE_INPUT_DIR).isDirectory()==false){
+			System.out.println("Directory does not exists : " + FILE_INPUT_DIR);
+			System.exit(0);
+		}
+		
+		if(new File(FILE_OUTPUT_DIR).isDirectory()==false){
+			System.out.println("Directory does not exists : " + FILE_OUTPUT_DIR);
+			System.exit(0);;
+		}
+		if(ADD_ML_FEATURES){
+			if(new File(FILE_ML_DIR).isDirectory()==false){
+				System.out.println("Directory does not exists : " + FILE_ML_DIR);
+				System.exit(0);;
+			}
+		}
+	}
+	
+	public void listFile(String folder, String ext) {
+
+		GenericExtFilter filter = new GenericExtFilter(ext);
+
+		File dirInput = new File(folder);
+
+
+		// list out all the file name and filter by the extension
+		String[] list = dirInput.list(filter);
+
+		if (list.length == 0) {
+			System.out.println("no files end with : " + ext);
+			return;
+		}
+
+		for (String file : list) {
+			String temp = new StringBuffer(FILE_INPUT_DIR).append(File.separator)
+					.append(file).toString();
+			System.out.println("file : " + temp);
+		}
+		
+		this.list = list;
+		
+		return;
+	}
+	
+	
+	/*-----------------------JSON-------------------------*/
+	private void convertJSONs(boolean addML, String mlDirecotoryPath) throws JsonParseException, JsonMappingException, IOException{
+		if(list.length == 0){
+			System.out.println("No JSONs to be converted");
+			return;
+		}
+		Map<String,InstanceData> istanceDataList = new HashMap<>();
+		List<InstanceDataMultiProvider> istanceDataMultiProviderList = new ArrayList<>();
+		for(String jsonPath : list){
+			InstanceData id = getJsonFromPath(jsonPath);
+			if(addML){
+				String mlFileName = jsonPath.split("_")[0]+".json";
+				File f = new File(FILE_ML_DIR+"/"+ mlFileName);
+				if(f.exists() && !f.isDirectory()) { 
+				   id.setMapJobMLProfiles(getMLProfileFromPath(mlFileName));
+				}else{
+					System.out.println("Not possible to add mlProfile. File not found:"+FILE_ML_DIR+"/"+ mlFileName);
+				}
+			}else{
+				System.out.println("MLProfiles Not added from external directory");
+			}
+			istanceDataList.put(jsonPath,id);
+		}
+		
+		for(Map.Entry<String,InstanceData> input : istanceDataList.entrySet()){
+			InstanceDataMultiProvider idmp = JsonMapper.ConvertJson(input.getValue());
+			mapper.writeValue(new File(FILE_OUTPUT_DIR + "/" + input.getKey()), idmp);
+			istanceDataMultiProviderList.add(idmp);
+		}
+		
+	}
+	
+	
+	private InstanceData getJsonFromPath(String path) throws JsonParseException, JsonMappingException, IOException{
+		File file = new File(FILE_INPUT_DIR+"/"+path);
+		InstanceData instanceData = mapper.readValue(file, InstanceData.class);
+		return instanceData;
+	}
+	
+	private JobMLProfilesMap getMLProfileFromPath(String path) throws JsonParseException, JsonMappingException, IOException{
+		File file = new File(FILE_ML_DIR+"/"+path);
+		JobMLProfilesMap mlProfiles = mapper.readValue(file, JobMLProfilesMap.class);
+		return mlProfiles;
+	}
+	
+	/*------------------------------------------------*/
+		
+	public class GenericExtFilter implements FilenameFilter {
+
+		private String ext;
+
+		public GenericExtFilter(String ext) {
+			this.ext = ext;
+		}
+
+		public boolean accept(File dir, String name) {
+			return (name.endsWith(ext));
+		}
+	}
+
+}
---
+++ src/JsonMapper.java
@@ -0,0 +1,157 @@
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import it.polimi.diceH2020.SPACE4Cloud.shared.inputData.InstanceData;
+import it.polimi.diceH2020.SPACE4Cloud.shared.inputData.JobClass;
+import it.polimi.diceH2020.SPACE4Cloud.shared.inputData.Profile;
+import it.polimi.diceH2020.SPACE4Cloud.shared.inputData.TypeVM;
+import it.polimi.diceH2020.SPACE4Cloud.shared.inputData.TypeVMJobClassKey;
+import it.polimi.diceH2020.SPACE4Cloud.shared.inputDataMultiProvider.ClassParameters;
+import it.polimi.diceH2020.SPACE4Cloud.shared.inputDataMultiProvider.ClassParametersMap;
+import it.polimi.diceH2020.SPACE4Cloud.shared.inputDataMultiProvider.InstanceDataMultiProvider;
+import it.polimi.diceH2020.SPACE4Cloud.shared.inputDataMultiProvider.JobMLProfilesMap;
+import it.polimi.diceH2020.SPACE4Cloud.shared.inputDataMultiProvider.JobProfile;
+import it.polimi.diceH2020.SPACE4Cloud.shared.inputDataMultiProvider.JobProfilesMap;
+import it.polimi.diceH2020.SPACE4Cloud.shared.inputDataMultiProvider.PrivateCloudParameters;
+import it.polimi.diceH2020.SPACE4Cloud.shared.inputDataMultiProvider.PublicCloudParameters;
+import it.polimi.diceH2020.SPACE4Cloud.shared.inputDataMultiProvider.PublicCloudParametersMap;
+import it.polimi.diceH2020.SPACE4Cloud.shared.inputDataMultiProvider.VMConfigurationsMap;
+
+public class JsonMapper {
+	
+	public static InstanceDataMultiProvider ConvertJson(InstanceData input){
+		InstanceDataMultiProvider output = new InstanceDataMultiProvider();
+		
+		output.setId(input.getId());
+		
+		if(input.getLstClass() != null && !input.getLstClass().isEmpty()){
+			output.setMapClassParameters(retrieveMapClassParameters(input.getLstClass()));
+		}
+		if(input.getMapJobMLProfiles() != null && input.getMapJobMLProfiles().getMapJobMLProfile() != null){
+			if(input.getMapJobMLProfiles().validate()){
+				output.setMapJobMLProfiles(retrieveMapJobMLProfiles(input.getMapJobMLProfiles()));
+			}else{
+				System.out.println("MLProfile not valid!");
+			}
+		}else{
+			System.out.println("- JobMlProfilesNotFound");
+		}
+		if(input.getMapProfiles() != null && !input.getMapProfiles().isEmpty()){
+			output.setMapJobProfiles(retrieveMapJobProfiles(input.getMapProfiles(), input.getProvider()));
+		}
+		if(input.getMapTypeVMs() != null && input.getMapTypeVMs().isPresent() && !input.getMapTypeVMs().get().isEmpty()){
+			output.setMapPublicCloudParameters(retrieveMapPublicCloudParameters(input.getMapTypeVMs(), input.getProvider()));
+		}
+		if(input.getMapVMConfigurations() != null && input.getMapVMConfigurations().isPresent() && !input.getMapVMConfigurations().get().getMapVMConfigurations().isEmpty() && input.getMapVMConfigurations().get().getMapVMConfigurations()!=null && input.getMapVMConfigurations().get().validate()){
+			output.setMapVMConfigurations(retrieveMapVMConfigurations(input.getMapVMConfigurations()));
+		}
+		if(input.getPrivateCloudParameters() != null && input.getPrivateCloudParameters().isPresent() && input.getPrivateCloudParameters().get().validate()){
+			output.setPrivateCloudParameters(retrievePrivateCloudParameters(input.getPrivateCloudParameters()));
+		}
+		
+		return output;
+	}
+	
+	private static ClassParametersMap retrieveMapClassParameters(List<JobClass> lstClass){
+		Map<String,ClassParameters> mapClassParameters = new HashMap<>();
+		for(JobClass jobClass : lstClass){
+			ClassParameters cp = new ClassParameters();
+			cp.setD(jobClass.getD());
+			cp.setHlow(jobClass.getHlow());
+			cp.setHup(jobClass.getHup());
+			cp.setPenalty(jobClass.getJob_penalty());
+			cp.setThink(jobClass.getThink());
+			cp.setM(jobClass.getM());
+			cp.setV(jobClass.getV());
+			mapClassParameters.put(jobClass.getId(), cp);
+		}
+		return new ClassParametersMap(mapClassParameters);
+	}
+	
+	private static JobProfilesMap retrieveMapJobProfiles(Map<TypeVMJobClassKey, Profile> mapProfiles, String provider){
+		Map<String, Map<String,Map<String,JobProfile>>> mapJobProfiles = new HashMap<>();
+		
+		
+		for(Map.Entry<TypeVMJobClassKey, Profile> entry : mapProfiles.entrySet()){
+			JobProfile p = new JobProfile();
+			
+			p.setNm(entry.getValue().getNm());
+    		p.setNr(entry.getValue().getNr());
+    		p.setCm(entry.getValue().getCm());
+    		p.setCr(entry.getValue().getCr());
+    		p.setMavg(entry.getValue().getMavg());
+    		p.setMmax(entry.getValue().getMmax());
+    		p.setRavg(entry.getValue().getRavg());
+    		p.setRmax(entry.getValue().getRmax());
+    		p.setSh1max(entry.getValue().getSh1max());
+    		p.setShtypavg(entry.getValue().getShtypavg());
+    		p.setShtypmax(entry.getValue().getShtypmax());
+    		
+    		if(mapJobProfiles.containsKey(entry.getKey().getJob())){
+    			if(mapJobProfiles.get(entry.getKey().getJob()).containsKey(provider)){
+    				mapJobProfiles.get(entry.getKey().getJob()).get(provider).put(entry.getKey().getTypeVM(), p);
+    			}else{
+        			Map<String,JobProfile> typeVMMap = new HashMap<>();
+        			typeVMMap.put(entry.getKey().getTypeVM(), p);
+        			mapJobProfiles.get(entry.getKey().getJob()).put(provider, typeVMMap);
+    			}
+    		}else{
+    			Map<String,Map<String,JobProfile>> providerMap = new HashMap<>();
+    			Map<String,JobProfile> typeVMMap = new HashMap<>();
+    			typeVMMap.put(entry.getKey().getTypeVM(), p);
+    			providerMap.put(provider,typeVMMap);
+    			mapJobProfiles.put(entry.getKey().getJob(), providerMap );
+    		}
+		}
+		return new JobProfilesMap(mapJobProfiles);
+	}
+	
+	private static PublicCloudParametersMap retrieveMapPublicCloudParameters(Optional<Map<String, List<TypeVM>>> mapTypeVMsO, String provider){
+		Map<String, List<TypeVM>> mapTypeVMs = mapTypeVMsO.get();
+		
+		Map<String, Map<String,Map<String,PublicCloudParameters>>> mapPublicCloudParameters = new HashMap<>();
+		
+		for(Map.Entry<String, List<TypeVM>> mapEntry : mapTypeVMs.entrySet()){
+			for(TypeVM lstEntry : mapEntry.getValue()){
+				
+				PublicCloudParameters p = new PublicCloudParameters();
+				p.setEta(lstEntry.getEta());
+				p.setR(lstEntry.getR());
+				
+				if(mapPublicCloudParameters.containsKey(mapEntry.getKey())){
+					if(mapPublicCloudParameters.get(mapEntry.getKey()).containsKey(provider)){
+						mapPublicCloudParameters.get(mapEntry.getKey()).get(provider).put(lstEntry.getId(), p);
+	    			}else{
+	        			Map<String,PublicCloudParameters> typeVMMap = new HashMap<>();
+	        			typeVMMap.put(lstEntry.getId(), p);
+	        			mapPublicCloudParameters.get(mapEntry.getKey()).put(provider, typeVMMap);
+	    			}
+				}else{
+					Map<String,Map<String,PublicCloudParameters>> providerMap = new HashMap<>();
+	    			Map<String,PublicCloudParameters> typeVMMap = new HashMap<>();
+	    			typeVMMap.put(lstEntry.getId(), p);
+	    			providerMap.put(provider,typeVMMap);
+	    			mapPublicCloudParameters.put(mapEntry.getKey(), providerMap);
+				}
+			}
+			
+		}
+		return new PublicCloudParametersMap(mapPublicCloudParameters);
+	}
+	
+	private static VMConfigurationsMap retrieveMapVMConfigurations(Optional<VMConfigurationsMap> mapVMConfigurationsO){
+		return  mapVMConfigurationsO.get();
+	}
+	
+	private static PrivateCloudParameters retrievePrivateCloudParameters(Optional<PrivateCloudParameters> privateCloudParameters){
+		return privateCloudParameters.get();
+	}
+	
+	private static JobMLProfilesMap retrieveMapJobMLProfiles(JobMLProfilesMap mapJobMLProfiles){
+		return mapJobMLProfiles;
+	}
+	
+}
